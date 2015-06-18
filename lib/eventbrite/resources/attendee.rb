02949f59545ceab63ab76520ab67a009c0768cce
Ben Greenberg <bsgreenb@gmail.com>
2015-06-17 17:23:04 -0700

    expand and print url

--- lib/eventbrite/resources/attendee.rb
+++ lib/eventbrite/resources/attendee.rb
@@ -3,13 +3,14 @@ module Eventbrite
     def self.retrieve(event_id, id, token=nil)
       instance = self.new(id, token)
       instance.event_id = event_id
-      instance.retrieve_options = {'expansion': 'promotional_code'}
       instance.refresh
       instance
     end
 
     def url
-      "/events/#{CGI.escape(self.event_id)}#{self.class.url}/#{CGI.escape(self.id)}"
+      print "HERE WITH "
+      print "/events/#{CGI.escape(self.event_id)}#{self.class.url}/#{CGI.escape(self.id)}?expansion=promotional_code"
+      "/events/#{CGI.escape(self.event_id)}#{self.class.url}/#{CGI.escape(self.id)}?expansion=promotional_code"
     end
   end
 end
